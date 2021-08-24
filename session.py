c5e65c5072c3331f967598fbb1a9870844bd15e3
Usr <87721641+tgusr@users.noreply.github.com>
2021-08-25 00:38:38 +0530

    Update session.py

--- session.py
+++ session.py
@@ -5,7 +5,4 @@ from telethon.errors import UserNotParticipantError
 import os
 session = os.environ.get("SESSION")
 
-class user():
-    client = TelegramClient(session, 5367566, "641c80fcd5206311afd85bf78580ca4f")
-    
-user.client.run_untill_disconnect()
+c = TelegramClient(session, 5367566, "641c80fcd5206311afd85bf78580ca4f")
